4b0ca3919d73e35a55e2a3326759509233d0965d
Dez Beberniss <dezmarie@gmail.com>
2021-11-13 18:01:31 -0800

    Simplify paths

--- _src/functions/login.js
+++ _src/functions/login.js
@@ -22,14 +22,13 @@ exports.handler = async function (event, context) {
   try {
     const response = await axios.post(endpoint, data, options);
     const access_token = response.data.access_token;
-    const privateUri = new RegExp(`/projects/pvt-*/*`);
 
     return {
       statusCode: 302,
       headers: {
         'Set-Cookie': `nf_jwt=${access_token}; Path=/; HttpOnly; Secure`,
         'Cache-Control': 'no-cache',
-        Location: redirect || privateUri,
+        Location: redirect || '/projects/private',
       },
     };
   } catch (error) {
