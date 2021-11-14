e65a14f3f7461f53e9901d674a285162c24e1f6f
Dez Beberniss <dezmarie@gmail.com>
2021-11-13 17:55:01 -0800

    Add functions & Axios for login redirection

---
+++ _src/functions/login.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+require("dotenv").config();
+
+exports.handler = async function (event, context) {
+  const { password } = new URLSearchParams(event.body);
+  const { redirect } = new URLSearchParams(event.headers.referer.split('?')[1]);
+
+  const endpoint = `${process.env.URL}/.netlify/identity/token`;
+
+  const data = new URLSearchParams({
+    grant_type: 'password',
+    username: process.env.LOGIN_EMAIL,
+    password: password,
+  }).toString();
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  try {
+    const response = await axios.post(endpoint, data, options);
+    const access_token = response.data.access_token;
+    const privateUri = new RegExp(`/pvt-*/*`);
+
+    return {
+      statusCode: 302,
+      headers: {
+        'Set-Cookie': `nf_jwt=${access_token}; Path=/; HttpOnly; Secure`,
+        'Cache-Control': 'no-cache',
+        Location: redirect || privateUri,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      statusCode: 302,
+      headers: {
+        'Cache-Control': 'no-cache',
+        Location: `/login/?redirect=${encodeURIComponent(redirect)}`,
+      },
+    };
+  }
+};
