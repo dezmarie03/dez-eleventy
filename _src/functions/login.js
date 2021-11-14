734ae2f1cc52fec804029ba9b036eec4585a6b1a
Dez Beberniss <dezmarie@gmail.com>
2021-11-13 19:42:35 -0800

    Use querystring instead of URLSearchParams for redirection

--- _src/functions/login.js
+++ _src/functions/login.js
@@ -1,16 +1,18 @@
+const querystring = require('querystring');
 const axios = require('axios');
 require("dotenv").config();
 
 exports.handler = async function (event, context) {
-  const { password } = new URLSearchParams(event.body);
+  const { password } = querystring.parse(event.body);
+  const { redirect } = querystring.parse(event.headers.referer.split('?')[0]);
 
   const endpoint = `${process.env.URL}/.netlify/identity/token`;
 
-  const data = new URLSearchParams({
+  const data = querystring.stringify({
     grant_type: 'password',
     username: process.env.LOGIN_EMAIL,
     password: password,
-  }).toString();
+  });
 
   const options = {
     headers: {
@@ -27,7 +29,7 @@ exports.handler = async function (event, context) {
       headers: {
         'Set-Cookie': `nf_jwt=${access_token}; Path=/; HttpOnly; Secure`,
         'Cache-Control': 'no-cache',
-        Location: '/projects/private',
+        Location: redirect || '/projects/private/',
       },
     };
   } catch (error) {
@@ -37,7 +39,7 @@ exports.handler = async function (event, context) {
       statusCode: 302,
       headers: {
         'Cache-Control': 'no-cache',
-        Location: `/login/`,
+        Location: `/login/?redirect=${encodeURIComponent(redirect)}`,
       },
     };
   }
