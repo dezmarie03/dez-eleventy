f0ec8a66cec48f69a93e1dd8fa4c48a2b5bedf8e
Dez Beberniss <dezmarie@gmail.com>
2021-11-17 16:47:40 -0800

    Add graphic to contact page; bump tsParticles version; refactor tsParticles implementations

--- _src/js/particles-glitter.js
+++ _src/js/particles-glitter.js
@@ -1,52 +1,58 @@
 // Glitter
 tsParticles.load("glitter", {
-  "detectRetina": true,
-  "fpsLimit": 60,
-  "backgroundMode": {
-    "enable": true,
-    "zIndex": 0
+  detectRetina: true,
+  fpsLimit: 60,
+  backgroundMode: {
+    enable: true,
+    zIndex: 0
   },
-  "emitters": {
-    "direction": "bottom",
-    "size": {
-      "width": 100,
-      "height": 0,
-    },
-    "position": {
-      "x": 0,
-      "y": 0,
-    },
-    "rate": {
-      "delay": 2,
-      "quantity": 300,
+  particles: {
+    number: {
+      value: 0,
     },
   },
-  "particles": {
-    "move": {
-      "angle": 90,
-      "direction": "bottom",
-      "enable": true,
-      "outMode": "out",
-      "speed": 3,
+  emitters: {
+    direction: "bottom",
+    size: {
+      width: 100,
+      height: 100,
     },
-    "color": {
-      "value": [
-        "#987008",
-        "#EEAC03",
-        "#FFE5A3",
-      ],
+    position: {
+      x: 0,
+      y: 0,
     },
-    "size": {
-      "random": {
-        "enable": true,
-        "minimumValue": 1,
+    rate: {
+      delay: 2,
+      quantity: 300,
+    },
+    particles: {
+      move: {
+        angle: 90,
+        direction: "bottom",
+        enable: true,
+        outMode: "out",
+        speed: 3,
       },
-      "value": 2,
-    },
-    "life": {
-      "duration": {
-        "sync": true,
-        "value": 10,
+      shape: "circle",
+      color: {
+        value: [
+          "#987008",
+          "#EEAC03",
+          "#FFE5A3",
+        ],
+      },
+      size: {
+        random: {
+          enable: true,
+          minimumValue: 1,
+        },
+        value: 2,
+      },
+      life: {
+        duration: {
+          sync: true,
+          value: 10,
+        },
       },
     },
   },
